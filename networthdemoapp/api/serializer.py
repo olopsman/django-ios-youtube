e8deae514c9215ee4ce81372f362dfd26bb127cd
Paulo Orquillo <paulo.orquillo@gmail.com>
2021-03-21 13:52:05 +1300

    Updated serializer

--- networthdemoapp/api/serializer.py
+++ networthdemoapp/api/serializer.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ['name', 'category', 'description', 'wealth_type', 'balance','created_at']
+        fields = ['id','name', 'category', 'description', 'wealth_type', 'balance','created_at']
