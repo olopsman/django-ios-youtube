ca53176e8345aabf162965d262f1963e06896033
Paulo Orquillo <paulo.orquillo@gmail.com>
2021-04-07 23:57:02 +1200

    added dj database connector

--- networthdemoapp/settings.py
+++ networthdemoapp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,6 +89,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+db_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_env)
 
 
 # Password validation
